ee1bbc48b06035cba994630229f13b3610b01a9b
sonalsk <sonalkushwaha039@gmail.com>
2021-07-01 20:36:35 +0530

    Added React-Bootstrap

--- client/src/routes/mode.js
+++ client/src/routes/mode.js
@@ -1,13 +1,15 @@
 /* ------ IMPORTING FILES ------- */
-import React from "react";
+import React from 'react';
 import {Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 function Mode() {
     return (
         <div>
             <h1>Choice</h1>
-            <Link to="/CreateRoom"><button> 1:1 </button></Link>
+            {/* <Link to="/CreateRoom"><button> 1:1 </button></Link> */}
             <button> Group </button>
+            <Button variant="primary" href='/CreateRoom'>1:1</Button>
         </div>
     );
 }
