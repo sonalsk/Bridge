6e33fc06b82e075019eda7b20a68e68c062a778a
sonalsk <sonalkushwaha039@gmail.com>
2021-07-02 20:21:53 +0530

    Changed the UI of One on One call

--- client/src/routes/createRoom.js
+++ client/src/routes/createRoom.js
@@ -2,7 +2,8 @@
 import React from "react";
 // importing uuid to create and have unique ID for each room
 import { v1 as uuid } from "uuid";
-
+import one2one from '../assets/OneToOne.png';
+import '../css/createRoom.css'
 
 /* ------ CREATING A ROOM ------ */
 
@@ -19,9 +20,22 @@ const CreateRoom = (props) => {
 
     return (
         <div>
-        <h1>One on One</h1>
-        <button> Schedule </button>
-        <button onClick = {create}> Create Room </button>
+            <section id="oo-lp">
+                <div class="row">
+                    <div class="col-md-6 c2">
+                        <img src={one2one} class="d-none d-md-block about-img-cr"></img>
+                    </div>
+                    <div class="col-md-6 align-self-center welcomeCR">
+                        <h1>One on One </h1>
+                        <h1>Video Call with Bridge</h1>
+                        <p>Join or Schedule a one to one <br></br> video call with your peers!</p>
+                        
+                        <button class="schedule" onClick={() => window.location.replace("/")}> Home </button>
+                        <button class="schedule"> Schedule Call </button>
+                        <button class="schedule" onClick = {create}> Instant Call </button>
+                    </div>
+                </div>
+            </section>
         </div>
         
     );
